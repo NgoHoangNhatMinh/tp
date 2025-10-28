f7f576166892c4c8d4dcc2e17e8f83c31418c494
Tan-Guan-Lin <37446380+Tan-Guan-Lin@users.noreply.github.com>
2025-10-28 16:22:30 +0800

    Fix Checkstyle Issues

--- src/main/java/seedu/address/logic/commands/ListPrescriptionsCommand.java
+++ src/main/java/seedu/address/logic/commands/ListPrescriptionsCommand.java
@@ -1,15 +1,15 @@
 package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
-import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PRESCRIPTIONS;
 import static seedu.address.logic.commands.ViewType.PRESCRIPTION_LIST;
+import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PRESCRIPTIONS;
 
 import seedu.address.model.Model;
 
 /**
  * Lists all prescriptions in the address book to the user.
  */
-public class ListPrescriptionsCommand extends Command{
+public class ListPrescriptionsCommand extends Command {
     public static final String COMMAND_WORD = "p-list";
 
     public static final String MESSAGE_SUCCESS = "Listed all prescriptions";
